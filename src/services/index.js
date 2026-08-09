ce916a1f92454b1bc606f458b7064c816413c635
OBA DOCERIA <OBADOCERIA@GMAIL.com>
2026-08-09 20:35:11 -0300

    feat: adiciona catalog service

---
+++ src/services/index.js
@@ -0,0 +1,13 @@
+export {
+  initializeCatalogService,
+  getCatalogData,
+  getCatalogViewModel,
+  getCatalogVersion,
+  getStore,
+  getSections,
+  getAllViewItems,
+  findViewItemById,
+  getConfigurableBoxes,
+  getFixedPriceItems,
+  clearCatalogServiceCache
+} from './catalog-service.js';
